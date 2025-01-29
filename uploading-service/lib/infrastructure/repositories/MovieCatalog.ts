b34bd295c35d137a873af9c3222384fa9044829d
hishammpsnhn <hishammpsn@gmail.com>
2025-01-29 12:40:20 +0530

    clean up

--- uploading-service/lib/infrastructure/repositories/MovieCatalog.ts
+++ uploading-service/lib/infrastructure/repositories/MovieCatalog.ts
@@ -1,19 +1,13 @@
-// src/infrastructure/repositories/MovieCatalogRepository.ts
 import { IEncodedFile, MovieCatalog } from "../../domain/entities/VideoCatalog";
 import { MovieCatalogModel } from "../database/models/catalog"; // Assuming you have a Mongoose model
 
 export class MovieCatalogRepository {
-  // Create a new catalog entry
   async create(movieCatalog: MovieCatalog): Promise<MovieCatalog> {
-    console.log('Creating movie catalog:')
     const movieCatalogDoc = new MovieCatalogModel(movieCatalog);
-    console.log(movieCatalogDoc)
     const savedCatalog = await movieCatalogDoc.save();
-    console.log("saved catalog: " + savedCatalog)
-    return savedCatalog.toDomain(); // Assuming you have a `toDomain` method
+    return savedCatalog.toDomain(); 
   }
 
-  // Update an existing catalog entry with encoded file details
   async update(id: string, encodedFiles: IEncodedFile[]): Promise<MovieCatalog> {
     const updatedCatalog = await MovieCatalogModel.findByIdAndUpdate(
       id,
@@ -25,8 +19,7 @@ export class MovieCatalogRepository {
       throw new Error(`Movie catalog with id ${id} not found`);
     }
 
-    return updatedCatalog.toDomain(); // Assuming you have a `toDomain` method
+    return updatedCatalog.toDomain(); 
   }
 
-  // Other repository methods like `findById`, `delete`, etc.
 }
