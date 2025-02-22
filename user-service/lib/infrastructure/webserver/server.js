1e59bd7196094bca4907a3ee4194287dc888fc65
hishammpsnhn <hishammpsn@gmail.com>
2025-02-22 21:35:00 +0530

    test

--- user-service/lib/infrastructure/webserver/server.js
+++ user-service/lib/infrastructure/webserver/server.js
@@ -3,20 +3,21 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "../../interface/routes/authRoutes.js";
-import profileRoutes from '../../interface/routes/profileRoutes.js';
-import morgan from 'morgan'
+import profileRoutes from "../../interface/routes/profileRoutes.js";
+import morgan from "morgan";
 dotenv.config();
 
 const createServer = async (metricsService) => {
   const app = express();
 
-  app.use(cors({
-    origin: ["http://localhost:5173", "http://viewnet.cfd"],
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-    credentials: true, 
-  }));
-  app.use(morgan('combined')); 
-
+  app.use(
+    cors({
+      origin: ["http://localhost:5173", "http://viewnet.cfd"],
+      methods: "GET,POST,PUT,DELETE,OPTIONS",
+      credentials: true,
+    })
+  );
+  app.use(morgan("combined"));
 
   app.use(cookieParser());
   app.use(express.json());
@@ -27,7 +28,11 @@ const createServer = async (metricsService) => {
 
   // Routes
   app.use("/api/user", authRoutes);
-  app.use('/api/user',  profileRoutes);
+  app.use("/api/user", profileRoutes);
+  app.get("/", (req, res) => {
+    console.log("welcome");
+    res.send("Welcome to the API!");
+  });
 
   return app;
 };
