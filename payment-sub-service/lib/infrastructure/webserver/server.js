b55cae4d616cbd94f106782800f9e76efa7d9aef
hishammpsnhn <hishammpsn@gmail.com>
2025-02-23 23:58:27 +0530

    issue of webhook

--- payment-sub-service/lib/infrastructure/webserver/server.js
+++ payment-sub-service/lib/infrastructure/webserver/server.js
@@ -2,24 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import subscriptionTypeRoute from "../../interface/routes/subscriptionTypeRoutes.js";
-import morgan from 'morgan'
-
+import morgan from "morgan";
 
 dotenv.config();
-
 
 const createServer = async () => {
   const app = express();
 
-
   // Middleware setup
   app.use(cookieParser());
-  app.use(morgan('combined'))
-
-  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan("combined"));
 
   app.use("/webhook", express.raw({ type: "application/json" }));
 
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
   // Routes
