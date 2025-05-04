336f5e95a05d31bca1cc52aa5a4b3f89158eebb0
hishammpsnhn <hishammpsn@gmail.com>
2025-05-04 21:27:35 +0530

    fix: series upload url modifeid

--- uploading-service/lib/use-cases/series/EpisodeUseCase.ts
+++ uploading-service/lib/use-cases/series/EpisodeUseCase.ts
@@ -23,7 +23,7 @@ export class EpisodeUseCase {
     }
 
     const baseUrl =
-      `https://eu-north-1.amazonaws.com/production.${environment.AWS_BUCKET_NAME}/hls`;
+      `https://s3.eu-north-1.amazonaws.com/production.${environment.AWS_BUCKET_NAME}/hls`;
 
     const resolutions = (
       ["1080p", "720p", "480p", "360p", "auto"] as const
